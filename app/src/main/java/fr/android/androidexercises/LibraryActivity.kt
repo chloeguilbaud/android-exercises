d811438d90438c06f02237a678945e3922c4d827
Chloe <44chloeguilbaud@gmail.com>
2018-11-15 15:57:06 +0100

    feat: Downloading image from internet using Picasso - exercice9

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -2,6 +2,9 @@ package fr.android.androidexercises
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -9,8 +12,15 @@ class LibraryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
 
+        val imageView = findViewById<ImageView>(R.id.downloadImageView)
+
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        Picasso
+                .get()
+                .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+                .centerCrop()
+                .into(imageView)
     }
 
 }
