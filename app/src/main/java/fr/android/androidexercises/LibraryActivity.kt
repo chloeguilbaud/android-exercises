278bd4b76353e6ae89e16def2de28f5fe11f7310
Chloe <44chloeguilbaud@gmail.com>
2018-11-16 11:59:31 +0100

    feat: Affichage des deux fragments en mode lanscape

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -1,7 +1,10 @@
 package fr.android.androidexercises
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.FrameLayout
 
 class LibraryActivity : AppCompatActivity(), Step0Fragment.OnNextStep0Listener {
 
@@ -16,12 +19,22 @@ class LibraryActivity : AppCompatActivity(), Step0Fragment.OnNextStep0Listener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
 
+        val landscape = resources.getBoolean(R.bool.landscape)
+
         // TODO replace Step0Fragment in containerFrameLayout
         // Le fragment est placer dans l'élément containerFrameLayout de la vue
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.containerFrameLayout, Step0Fragment())
                 .commit()
+        findViewById<FrameLayout>(R.id.containerFrameLayout2).visibility = View.INVISIBLE
+
+        if(landscape) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.containerFrameLayout2, Step1Fragment())
+                    .commit()
+            findViewById<FrameLayout>(R.id.containerFrameLayout2).visibility = View.VISIBLE
+        }
 
     }
 
